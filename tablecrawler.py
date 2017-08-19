efe0be6c7628bd55fdf883edc12a89a7117a8da2
reiser <admin@Leuven.local>
2017-08-19 11:33:11 +0800

    add tablecrawler

---
+++ tablecrawler.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+import logging
+import pyodbc
+import pprint
+
+'''
+*args tuple
+
+url
+id
+value
+
+'''
+def get_tables(*args):
+    url,id,value= args;
+    #fake useragent in http request headers
+    httpheaders = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    html=requests.get(url, headers=httpheaders)
+   
+    if __debug__:
+        if not html.status_code == 200: raise AssertionError
+
+    soup=BeautifulSoup(html.text,"lxml")
+    logging.debug(soup)
+
+    #get table by ID
+    table = soup.find(lambda tag: tag.name=='table' and tag.has_attr(id) and tag[id]==value)
+ 
+    #looks stupid
+    data = list()
+    rows = list()
+    cols = list()
+    table_headers = list()
+    json_dict = {}
+    for tx in table.find_all('th'):
+        table_headers.append(tx.text.strip()) 
+    data.append(table_headers)
+    rows=table.find_all('tr')
+    for row in rows:
+            cols=row.find_all('td')
+            cols = [ele.text.strip() for ele in cols if ele]
+            data.append([ele for ele in cols if ele]) # Get rid of empty values
+    
+    return data
+
+def export_json(data):
+    json_dict={}
+    heading = data[0]
+    for row in data [1:]:
+        for col_header, data_colume in zip(heading,row):
+            json_dict.setdefault(col_header,[]).append(data_colume);
+    return json.dumps(json_dict)
+
+def traverse_list(data):
+    cells = list()
+    for cells in data:
+       print (cells)
