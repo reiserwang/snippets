fb796afa780f373f3fe4ecd9ec9f8bd8af776149
reiser <pingchi.wang@gmail.com>
2017-08-25 10:45:52 +0800

    add data-science.py

---
+++ data_science.py
@@ -0,0 +1,55 @@
+import math, random
+
+def sum_of_squares(v):
+    """computes the sum of squared elements in v"""
+    return sum(v_i ** 2 for v_i in v)
+
+def minimize_stochastic(target_fn, gradient_fn, x, y, theta_0, alpha_0=0.01):
+    """randomized gradient descent"""
+    data = list(zip(x, y))
+    theta = theta_0                             # initial guess
+    alpha = alpha_0                             # initial step size
+    min_theta, min_value = None, float("inf")   # the minimum so far
+    iterations_with_no_improvement = 0
+
+    # if we ever go 100 iterations with no improvement, stop
+    while iterations_with_no_improvement < 100:
+        value = sum( target_fn(x_i, y_i, theta) for x_i, y_i in data )
+
+        if value < min_value:
+            # if we've found a new minimum, remember it
+            # and go back to the original step size
+            min_theta, min_value = theta, value
+            iterations_with_no_improvement = 0
+            alpha = alpha_0
+        else:
+            # otherwise we're not improving, so try shrinking the step size
+            iterations_with_no_improvement += 1
+            alpha *= 0.9
+
+        # and take a gradient step for each of the data points
+        for x_i, y_i in in_random_order(data):
+            gradient_i = gradient_fn(x_i, y_i, theta)
+            theta = vector_subtract(theta, scalar_multiply(alpha, gradient_i))
+
+    return min_theta
+
+"""
+** Multiple Regression **
+"""
+def squared_error(x_i, y_i, beta):
+    return error(x_i, y_i, beta) ** 2
+
+def squared_error_gradient(x_i, y_i, beta):
+    """the gradient corresponding to the ith squared error term"""
+    return [-2 * x_ij * error(x_i, y_i, beta)
+            for x_ij in x_i]
+
+def estimate_beta(x, y):
+    '''For multiple regression'''
+    beta_initial = [random.random() for x_i in x[0]]
+    return minimize_stochastic(squared_error,
+                               squared_error_gradient,
+                               x, y,
+                               beta_initial,
+                               0.001)
