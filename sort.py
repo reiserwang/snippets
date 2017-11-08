02908d94ee5b96b72e4f4f5550d6768438585a1e
Reiser <rewang@microsoft.com>
2017-11-08 12:44:27 +0800

    sort

--- sort.py
+++ sort.py
@@ -12,14 +12,14 @@ insertion_sort(random_items)
 print 'After : ', random_items
 
 def bubble_sort(items):
-    """Implementation of bubble sort"""
+    """Implementation of bubble sort. Complexity O(n^2)"""
     for i in range(len(items)):
                 for j in range(len(items)-1-i):
                         if items[j] > items[j+1]:
                                 items[j], items[j+1] = items[j+1], items[j]     # Swap!
  
 def insertion_sort(items):
-        """ Implementation of insertion sort """
+        """ Implementation of insertion sort.  Complexity O(n^2) """
         for i in range(1, len(items)):
                 j = i
                 while j > 0 and items[j] < items[j-1]:
@@ -27,10 +27,9 @@ def insertion_sort(items):
                         j -= 1
 
 def merge_sort(items):
-        """ Implementation of mergesort """
-        if len(items) &gt; 1:
- 
-                mid = len(items) / 2        # Determine the midpoint and split
+        """ Implementation of mergesort. Complexity O(n logn) """
+        if len(items) > 1:
+                quickmid = len(items) / 2        # Determine the midpoint and split
                 left = items[0:mid]
                 right = items[mid:]
  
@@ -53,7 +52,7 @@ def merge_sort(items):
                                 raise Exception('Could not merge, sub arrays sizes do not match the main array')
  
  def quick_sort(items):
-        """ Implementation of quick sort """
+        """ Implementation of quick sort. Complexity O(n logn) """
         if len(items) > 1:
                 pivot_index = len(items) / 2
                 smaller_items = []
@@ -72,7 +71,7 @@ def merge_sort(items):
  
 
 def heap_sort(items):
-        """ Implementation of heap sort """
+        """ Implementation of heap sort. Complexity O(n logn) """
         '''
         Python. To truly understand haepsort, one must implement the heapify() function themselves. 
         This is certainly one obvious area of improvement in this implementation.
